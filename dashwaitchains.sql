bde4fa2da723d751de375cc2ef125ea8b08a8803
Mhouri <32038462+Mhouri@users.noreply.github.com>
2024-03-11 09:29:00 +0100

    Add files via upload

---
+++ dashwaitchains.sql
@@ -0,0 +1,1 @@
+@dash_wait_chains2 instance_number||':'||session_id||':'||NVL(sql_id,'{sql_id}')||':'||sql_opname||':'||program2||event2 1=1 "timestamp'2020-&from'" "timestamp'2020-&to'"
---
+++ dashwaitchainsmho.sql
@@ -0,0 +1,2 @@
+------  ./dashwaitchains2.sql ------------------------
+@dash_wait_chains2 instance_number||':'||session_id||':'||NVL(sql_id,'{sql_id}')||':'||sql_opname||':'||program2||event2 session_type='FOREGROUND' "timestamp'2020-&from'" "timestamp'2020-&to'"
